d2e0aceaaa0430e1bc407b462d60f7711c4349c7
RaduAdumitroaei <radueugen84@yahoo.com>
2024-01-07 16:49:24 +0100

    small

--- comwell-backend-main/src/main.ts
+++ comwell-backend-main/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // Replace with your front-end URL
+    origin: 'http://localhost:3000', // Replace with your front-end URL
     credentials: true,
   });
 
