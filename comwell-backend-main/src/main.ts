f8f8cc53b353c761202216e4a0a417c7c2eb87e9
mhi1001 <35073349+mhi1001@users.noreply.github.com>
2024-01-09 21:19:56 +0100

    cors

--- comwell-backend-main/src/main.ts
+++ comwell-backend-main/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Replace with your front-end URL
+    origin: ['http://localhost:3000','http://127.0.0.1:3000'], // Replace with your front-end URL
     credentials: true,
   });
 
